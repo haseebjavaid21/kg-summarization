ca071ac5e3f84ebf33ac9f141f5a86d82d8fa719
Haseeb Javaid <haseebjavaid21@gmail.com>
2020-05-15 19:20:47 +0200

    Re-Formatting the Selector Flow

--- KGSMRSTN/src/main/java/org/dice/kgsmrstn/selector/AbstractSummarizationSelector.java
+++ KGSMRSTN/src/main/java/org/dice/kgsmrstn/selector/AbstractSummarizationSelector.java
@@ -67,48 +67,10 @@ public abstract class AbstractSummarizationSelector implements TripleSelector {
 		this.endpoint = endpoint;
 	}
 
-	protected List<Statement> getResources(Set<String> classes, String clazz,int topk) {
+	protected List<Statement> getResources(Set<String> classes, String clazz, int topk) {
 
-            //Reading from file part
-            try {
-            	Model m = ModelFactory.createDefaultModel() ;
-                // read into the model.
-                m.read("D:\\Project Data\\persondata_en.ttl\\persondata_en4.ttl") ;
-                System.out.println(m.size());
-                StmtIterator st = m.listStatements();
-
-                while (st.hasNext()) {
-    				// get the value of the variables in the select clause
-    				try {
-    					Statement et = st.next();
-    					String s = et.getSubject().toString();
-    					String p = et.getPredicate().toString();
-    					String o = et.getObject().toString();
-
-    					Node curr_s = new Node(s, 0, 0, ALGORITHM);
-    					Node curr_o = new Node(o, 0, 1, ALGORITHM);
-    					
-    					if (!(g.containsEdge(p) && g.containsVertex(curr_s)))
-    						g.addEdge(g.getEdgeCount() + ";" + p, curr_s, curr_o);
-
-    				} catch (Exception e) {
-    					e.printStackTrace();
-    				}
-    			}
-            } catch(Exception ex) {
-                System.out.println("Unable to Parse");
-            }
-
- 		List<Node> allNodesRanked = new ArrayList<Node>();
- 		allNodesRanked.addAll(g.getVertices());
-
-		List<Node> highRankNodes = new ArrayList<Node>();
-		for (Node node : allNodesRanked) {
-			if (node.getLevel() == 0 && !highRankNodes.contains(node)) {
-				highRankNodes.add(node);
-			}
-		}
-
+		this.readFromTTL("D:\\Project Data\\persondata_en.ttl\\persondata_en4.ttl");
+		
 		try {
 			HITSAlgorithm ht = new HITSAlgorithm();
 			ht.runHits(g, 10);
@@ -116,66 +78,10 @@ public abstract class AbstractSummarizationSelector implements TripleSelector {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		ArrayList<Node> orderedList = new ArrayList<Node>();
-		orderedList.addAll(g.getVertices());
-		Collections.sort(orderedList, new Comparator<Node>() {
 
-			public int compare(Node o1, Node o2) {
-				// TODO Auto-generated method stub
-				return Double.compare(o1.getAuthorityWeight(), o2.getAuthorityWeight());
-			}
-		});
-		
-		Collections.reverse(orderedList);
-		
-		int p = 0;
-		List<Node> temp = new ArrayList<>();
-		for (Node node_iter : orderedList) {
-
-			if(highRankNodes.contains(node_iter)) {
-				temp.add(node_iter);
-				System.out.println("Weight is "+node_iter+" "+node_iter.getAuthorityWeight());
-				p=p+1;
-			}	
-
-			if(p>10)
-				break;
-
-		}
-		
-		Model model = ModelFactory.createDefaultModel();
-		//form the triples of all the top nodes 
-
-		for(Node nodetemp: temp) {
-			Collection<Node> neighbourNodes1 = g.getNeighbors(nodetemp);
-			List<Node> neighbourNodes = new ArrayList<>();
-			neighbourNodes.addAll(neighbourNodes1);
-			Collections.sort(neighbourNodes, new Comparator<Node>() {
-
-				@Override
-				public int compare(Node o1, Node o2) {
-					// TODO Auto-generated method stub
-					return Double.compare(o1.getHubWeight(), o2.getHubWeight());
-				}
-			});
-			
-			for(Node succesorNode : neighbourNodes ) {
-
-				String sub = nodetemp.getCandidateURI();
-				String pred = g.findEdge(nodetemp, succesorNode);
-				String object = succesorNode.getCandidateURI();
-
-				if(pred!= null){
-					Resource subject = model.createResource(sub);
-					Property predicate = model.createProperty(pred.split(";")[1]);
-					model.add(subject, predicate, object);
-				}
-			}
-		}
-
+		List<Node> processedHighRankNodes = this.getHighRankNdes();
 		System.out.println("Finished");
-		return sortStatements(model.listStatements());
+		return sortStatements(this.generateList(processedHighRankNodes).listStatements());
 	}
 
 	private List<Node> runPageRank(DirectedSparseGraph<Node, String> g) {
@@ -228,5 +134,107 @@ public abstract class AbstractSummarizationSelector implements TripleSelector {
 		List<Statement> result = new ArrayList<Statement>(statements);
 		Collections.sort(result, new StatementComparator());
 		return result;
+	}
+	
+	protected List<Node> getHighRankNdes() {
+		List<Node> allNodesRanked = new ArrayList<Node>();
+		List<Node> NodeIteration = new ArrayList<>();
+		
+		allNodesRanked.addAll(g.getVertices());
+
+		List<Node> highRankNodes = new ArrayList<Node>();
+		for (Node node : allNodesRanked) {
+			if (node.getLevel() == 0 && !highRankNodes.contains(node)) {
+				highRankNodes.add(node);
+			}
+		}
+		
+		int p = 0;
+		for (Node node_iter : this.sortAndReverse(g)) {
+			if (highRankNodes.contains(node_iter)) {
+				NodeIteration.add(node_iter);
+				System.out.println("Weight is " + node_iter + " " + node_iter.getAuthorityWeight());
+				p = p + 1;
+			}
+
+			if (p > 10)
+				break;
+
+		}
+		return NodeIteration;
+		
+	}
+	
+	protected void readFromTTL(String ttl_path) {
+		Model m = ModelFactory.createDefaultModel();
+		// read into the model.
+		m.read(ttl_path);
+		StmtIterator st = m.listStatements();
+		while (st.hasNext()) {
+			// get the value of the variables in the select clause
+			try {
+				Statement et = st.next();
+				String s = et.getSubject().toString();
+				String p = et.getPredicate().toString();
+				String o = et.getObject().toString();
+
+				Node curr_s = new Node(s, 0, 0, ALGORITHM);
+				Node curr_o = new Node(o, 0, 1, ALGORITHM);
+
+				if (!(g.containsEdge(p) && g.containsVertex(curr_s)))
+					g.addEdge(g.getEdgeCount() + ";" + p, curr_s, curr_o);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	protected Model generateList(List<Node> temp) {
+		Model model = ModelFactory.createDefaultModel();
+		// form the triples of all the top nodes
+
+		for (Node nodetemp : temp) {
+			Collection<Node> neighbourNodes1 = g.getNeighbors(nodetemp);
+			List<Node> neighbourNodes = new ArrayList<>();
+			neighbourNodes.addAll(neighbourNodes1);
+			Collections.sort(neighbourNodes, new Comparator<Node>() {
+
+				@Override
+				public int compare(Node o1, Node o2) {
+					// TODO Auto-generated method stub
+					return Double.compare(o1.getHubWeight(), o2.getHubWeight());
+				}
+			});
+
+			for (Node succesorNode : neighbourNodes) {
+
+				String sub = nodetemp.getCandidateURI();
+				String pred = g.findEdge(nodetemp, succesorNode);
+				String object = succesorNode.getCandidateURI();
+
+				if (pred != null) {
+					Resource subject = model.createResource(sub);
+					Property predicate = model.createProperty(pred.split(";")[1]);
+					model.add(subject, predicate, object);
+				}
+			}
+		}
+		return model;
+	}
+	
+	protected ArrayList<Node> sortAndReverse(DirectedSparseGraph<Node, String>  g) {
+		ArrayList<Node> orderedList = new ArrayList<Node>();
+		orderedList.addAll(g.getVertices());
+		Collections.sort(orderedList, new Comparator<Node>() {
+
+			public int compare(Node o1, Node o2) {
+				// TODO Auto-generated method stub
+				return Double.compare(o1.getAuthorityWeight(), o2.getAuthorityWeight());
+			}
+		});
+
+		Collections.reverse(orderedList);
+		return orderedList;
+		
 	}
 }
